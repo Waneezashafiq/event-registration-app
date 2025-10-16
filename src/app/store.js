aebd1f16e56b46633e6849292ba3d8c7e6f6a255
Waneeza Shafiq <waneezashafiq@gmail.com>
2025-10-16 14:29:40 +0500

    Add Redux store configuration

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import eventsReducer from '../features/eventsSlice';
-import registrationsReducer from '../features/registrationsSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import eventsReducer from "../features/eventsSlice";
+import registrationsReducer from "../features/registrationsSlice";
 
 export const store = configureStore({
   reducer: {
     events: eventsReducer,
-    registrations: registrationsReducer
-  }
+    registrations: registrationsReducer,
+  },
 });
+
+export default store;
